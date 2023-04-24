8122d8650e02f4c5963f12d352ff7c7b83501daa
w83ll43 <sherilliwlkins258@gmail.com>
2023-04-24 20:38:51 +0800

    新建项目

---
+++ src/main/java/com/w83ll43/openapi/OpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.w83ll43.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiApplication.class, args);
+    }
+
+}
