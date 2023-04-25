dd3b591a02a5ac3ae12ab1c1f1b8ee55e945bbf9
w83ll43 <sherilliwlkins258@gmail.com>
2023-04-25 22:22:25 +0800

    增加过滤器

--- src/main/java/com/w83ll43/openapi/MainApplication.java
+++ src/main/java/com/w83ll43/openapi/MainApplication.java
@@ -1,13 +1,43 @@
 package com.w83ll43.openapi;
 
+import com.w83ll43.openapi.filter.LoginCheckFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
+// 过滤器扫描
+@ServletComponentScan
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    /**
+     * 通过 FilterRegistrationBean 注册过滤器
+     * 函数名随意
+     * 如果需要注册多个过滤器 则创建多个 Bean 即可
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+
+        // 设置过滤器
+        registration.setFilter(new LoginCheckFilter());
+
+        // 设置过滤器名称
+        registration.setName("loginCheckFilter");
+
+        // 设置拦截路径
+        registration.addUrlPatterns("/*");
+
+        // 设置顺序（权重）使用这个方法可以配置过滤链
+        registration.setOrder(10);
+        return registration;
     }
 
 }
---
+++ src/main/java/com/w83ll43/openapi/filter/LoginCheckFilter.java
@@ -0,0 +1,81 @@
+package com.w83ll43.openapi.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.w83ll43.openapi.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Slf4j
+// filterName 过滤器的名字 urlPatterns 需要拦截的请求
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
+public class LoginCheckFilter implements Filter {
+
+    // 路径匹配器
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        log.info("loginCheckPatterns 过滤器初始化");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        // 1、获取本次请求的 URI
+        String uri = request.getRequestURI();
+
+        // 定义不需要处理的请求路径
+        String[] urls = new String[]{
+                "/user/login",
+        };
+
+        // 2、判断本次请求是否需要处理
+        boolean check = check(urls, uri);
+
+        // 3、如果不需要处理则直接放行
+        if (check) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 4、判断登录状态 如果已经登录则直接放
+        if (request.getSession().getAttribute("user_login_status") != null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 5、如果未登录则返回未登录结果 如果未登录则返回未登录结果 通过输出流方式向客户端响应数据
+        response.getWriter().write(JSON.toJSONString(Result.error("用户未登录")));
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+        log.info("loginCheckPatterns 过滤器销毁");
+    }
+
+    /**
+     * 匹配路径
+     * @param urls
+     * @param uri
+     * @return
+     */
+    private boolean check(String[] urls, String uri) {
+        for (String url : urls) {
+            if (PATH_MATCHER.match(url, uri)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
